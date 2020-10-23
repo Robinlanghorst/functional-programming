91be710d4ebb302cc2630027f70c27777a2202d2
Robinlanghorst <robinlanghorst2@hotmail.com>
2020-10-23 15:27:43 +0200

    Update script.js

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,25 +1,24 @@
 // FIRST NEW: oogKleur to UpperCase with arrow function
 let surveyAnswers = data // requires data from data.js
-console.log("Survey Answers are:", surveyAnswers)
+console.log("Survey answers:", surveyAnswers)
 
-const colomnName = "oogKleur"
-const hexBrown = "663300"
-const hexBlue = "0099ff"
-const hexGreen = "009900"
-const rgbCode = "(139,69,19)"
+var columnName = "watWilJeWorden"
+var frontendDev = "Frontend Developer"
+/* var hexBlue = "0099ff"
+var hexGreen = "009900"
+var rgbCode = "(139,69,19)" */
 
 // Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function componentToHex (c) {
+/* function componentToHex (c) {
     let hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
     return componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+} */
 
-// Arrow function
-const replaceAll = surveyAnswers.map(answer => answer[colomnName]
+/* const replaceAll = surveyAnswers.map(answer => answer[colomnName]
     .replace("#", "")
     .replace(" ", "")
     .toUpperCase()
@@ -33,52 +32,39 @@ const replaceAll = surveyAnswers.map(answer => answer[colomnName]
     .replace(".", ",")
     .replace(rgbCode, (rgbToHex(139,69,19)))
     .replace("", "#")
+); */
+
+// Arrow function 
+var replaceAll = surveyAnswers.map(answer => answer[columnName]
+        .toUpperCase()
+
+    
+    /*     .replace("FRONTENDDEVELOPER", frontendDev)
+        .replace("FRONT-ENDDEVELOPER*", frontendDev)
+       .replace("FRONTEND/WEBDEVELOPER", frontendDev) */
+    /* .toUpperCase() */
+    /* .replace("BRUIN", hexBrown) */
+    // Replace blue and lightblue for 1 hex
+   /*  .replace("BLAUW", hexBlue)
+    .replace("LICHT", "")
+    .replace("GROEN", hexGreen) */
+    // Replace RGB for hex
+    /* .replace("RGB", "")
+    .replace(".", ",")
+    .replace(rgbCode, (rgbToHex(139,69,19)))
+    .replace("", "#") */
 );
+function spelling(input) {
+    let frontGrammar = ["FRONTEND", "FRONTENDER", "FRONT-END"]
 
-// IF ELSE BLUE
-// const replaceBlue = surveyAnswers.filter(answer => {
-//     if(answer[colomnName].includes("BLUE")){
-//         console.log('Er staat blauw in!')
-//     } else {
-//         console.log('Er staat geen blauw in!')
-//     }
-// })
+    if (input === "FRONTENDDEVELOPER")
+    return input
 
-// Regular function
-// const replaceToUpperCase = surveyAnswers.map(function(answer){
-//     return answer[colomnName].toLowerCase();
-// })
 
+else if (input !== "FRONTENDDEVELOPER"){
+    return input.replace(input, "FRONTENDDEVELOPER")
+}
+}
+spelling(input);
 console.log(replaceAll)
 
-// FIRST OLD (WORKS)
-// let surveyAnswers = data // requires data from data.js
-// console.log("Survey Answers are:", surveyAnswers)
-
-// const colomnName = "oogKleur"
-// const specificAnswer = surveyAnswers.map(answer => answer[colomnName].toUpperCase())
-
-// console.log("Eye colors: ", specificAnswer)
-
-// SECOND: filter the data with only oogKleur using a function
-// const surveyAnswers = data
-// const colomnName = "oogKleur"
-
-// let lijstAntwoorden = getAnswersForQuestion(surveyAnswers, colomnName)
-
-// console.log("lijstAntwoorden: ", lijstAntwoorden)
-
-// // returns all answers for a specific question in an Array
-// function getAnswersForQuestion(answers, question){ 
-// 	if (answers.length < 1){
-// 		console.error("Array must have items in it")
-// 		return
-// 	}
-// 	let answersForQuestion = []
-// 	for (answer of answers){
-// 		answersForQuestion.push(answer[question])
-// 	}
-// 	return answersForQuestion //array van antwoorden voor een specifieke vraag
-// }
-
-// Bron: https://github.com/Razpudding/selecting-data-json/blob/main/index.js
